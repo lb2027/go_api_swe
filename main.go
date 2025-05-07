d975b023c58d9c47693505f146aa632eb026abfa
Dwiki Yulian <79854897+qjvxvL@users.noreply.github.com>
2025-05-07 12:54:12 +0700

    go_selesai_beta

--- main.go
+++ main.go
@@ -18,18 +18,19 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	// Connect to the database
 	mux.Handle("/swagger/", httpSwagger.WrapHandler)
 	mux.HandleFunc("/login", database.API_generateJWT)
 	mux.HandleFunc("/register", database.API_register)
 
 	// Protected routes (pakai middleware JWT)
-	// CRUD User
+	// CRUD User (4 endpoints)
 	mux.Handle("/selectuser", database.MiddleWare(database.Api_selectAllData))
 	mux.Handle("/adduser", database.MiddleWare(database.API_add))
 	mux.Handle("/deleteuser", database.MiddleWare(database.Api_deleteUser))
 	mux.Handle("/updateuser", database.MiddleWare(database.Api_updateUser))
 
-	// CRUD Produk
+	// CRUD Produk (6 endpoints)
 	mux.Handle("/selectproduk", database.MiddleWare(database.Api_selectAllProduk))
 	mux.Handle("/addproduk", database.MiddleWare(database.API_addProduk))
 	mux.Handle("/deleteproduk", database.MiddleWare(database.Api_deleteProduk))
@@ -37,7 +38,7 @@ func main() {
 	mux.Handle("/selectProdukById", database.MiddleWare(database.Api_selectProdukById))
 	mux.Handle("/soldproduk", database.MiddleWare(database.Api_soldProduk))
 
-	// Transaksi
+	// Transaksi endpoints (7 endpoints)
 	mux.Handle("/selecttransaksi", database.MiddleWare(database.Api_selectAllTransaksi))
 	mux.Handle("/addtransaksi", database.MiddleWare(database.Api_addTransaksi))
 	mux.Handle("/displayhistory", database.MiddleWare(database.GetTransactionHistory))
@@ -49,7 +50,7 @@ func main() {
 	// Absensi
 	mux.Handle("/addabsensi", database.MiddleWare(database.Api_addAbsensi)) // POST
 
-	// Staff management endpoints
+	// Staff management endpoints (11 endpoints)
 	mux.Handle("/staff", database.MiddleWare(database.Api_getAllStaff))
 	mux.Handle("/staff/", database.MiddleWare(database.Api_getStaffByID))
 	mux.Handle("/addstaff", database.MiddleWare(database.Api_addStaff))
@@ -60,6 +61,15 @@ func main() {
 	mux.Handle("/searchstaff", database.MiddleWare(database.Api_searchStaff))
 	mux.Handle("/staffstats", database.MiddleWare(database.Api_getStaffStats))
 	mux.Handle("/makemestaff", database.MiddleWare(database.Api_getStaff))
+	mux.Handle("/getabsensi", database.MiddleWare(database.Api_getAttendanceLogs))
+
+	// Gaji management endpoints (4 endpoints)
+	mux.Handle("/getgaji", database.MiddleWare(database.Api_GetAllGaji)) // GET
+	mux.Handle("/addgaji", database.MiddleWare(database.Api_AddGaji)) // POST
+	mux.Handle("/updategaji", database.MiddleWare(database.Api_UpdateGaji)) // PUT
+	mux.Handle("/deletegaji", database.MiddleWare(database.Api_DeleteGaji)) // DELETE
+
+
 
 	// haruse keluar lo ini
 	// CORS setup
